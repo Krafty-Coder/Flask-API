be939d0567fb3d94720b7137e32d363a54cfe423
Krafty-Coder <agtychefortuna@gmail.com>
2018-03-26 19:50:18 +0300

    [Feature #156255215] update files and add register api request

---
+++ app/book_model.py
@@ -0,0 +1,15 @@
+books = []
+
+
+class Book:
+
+    def __init__(self, title, author, book_number, publisher):
+        self.title = title
+        self.author = author
+        self.book_number = book_number
+        self.publisher = publisher
+
+    def add_book(self):
+        pass
+
+
